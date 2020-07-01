5dd02a800d141e80c2baf396cb242091b729c2d9
tajahouse <ahlliahtaja@yahoo.com>
2020-07-01 10:12:26 -0700

    Still debugging

--- fe-medcab/src/components/User/Favorites.js
+++ fe-medcab/src/components/User/Favorites.js
@@ -13,9 +13,10 @@ const Favorites = ({ favorites }) =>{
             {favorites.map(strainName =>{
                 console.log({strainName})
                 return(
-                    <Link to={`/strains/${strainName.id}`} key={strainName.id} className="favorite-current">
-                      <Strain />
-                    </Link>
+                    <h4>{strainName.name}</h4>
+                    // <Link to={`/strains/${strainName.id}`} key={strainName.id} className="favorite-current">
+                      /* <Strain /> */
+                    // </Link>
                     
                 );
             })}
--- fe-medcab/src/components/User/Strains.js
+++ fe-medcab/src/components/User/Strains.js
@@ -4,59 +4,59 @@ import Strain from './Strain'
 import { useDispatch, connect } from "react-redux"
 import {getStrains} from "../../actions"
 
-// const Strains = ({ strains, isFetching, addToFavorites }) =>{
-// console.log(isFetching)
+const Strains = ({ strains, isFetching, addToFavorites }) =>{
+console.log(isFetching)
+    return(
+        <div className="strains-list">
+            {isFetching && (<>{strains.map(i => 
+            {return <div onClick={()=> {addToFavorites(i)}}>
+                 {/* <h5 className="strainNames"> Strain Name: {i.name} <br/> Strain Race: {i.race} </h5> */}
+                 
+             {/* <Link key={i.id} to={`/strains/${i.id}`}> */}
+            Strain Name: {i.name} <br/> Strain Race: {i.race} 
+                {/* <Strain strainName={i.name} strainRace={i.race}/> */}
+            {/* </Link> */}
+             </div>
+             })}</>)
+             }                    
+        </div>
+    )
+}
+
+// const Strains = props =>{
+//     const dispatch = useDispatch();
+//     const fetchStrains = e =>{
+//         e.preventDefault();
+//         dispatch(getStrains())
+//     }
+//     console.log("Strains component", props.strains)
+//     console.log("add to favorites", props.addToFavorites)
 //     return(
-//         <div className="strains-list">
-//             {isFetching && (<>{strains.map(i => 
-//             {return <div onClick={()=> {addToFavorites(i)}
+//         <div>
+//             <h1>Medical Cabinet Strains:</h1>
+//             {props.strains.map(i => 
+//             {return <div onClick={()=> {props.addToFavorites(i)}
                 
 //             }>
 //                  {/* <h5 className="strainNames"> Strain Name: {i.name} <br/> Strain Race: {i.race} </h5> */}
                  
 //             {/* <Link key={i.id} to={`/strains/${i.id}`}> */}
-//             Strain Name: {i.name} <br/> Strain Race: {i.race}
+//             {/* Strain Name: {i.name} <br/> Strain Race: {i.race} */}
 //                 {/* <Strain strain={i}/> */}
 //             {/* </Link> */}
-//              </div>})}</>)}                    
+//              </div>})}
+//             <Strain strains={props.strains} addToFavorites={props.addToFavorites}/>
+//          {/* <button classname="fetch_button" onClick= {fetchStrains}>Strains</button> */}
 //         </div>
 //     )
 // }
 
-const Strains = props =>{
-    const dispatch = useDispatch();
-    const fetchStrains = e =>{
-        e.preventDefault();
-        dispatch(getStrains())
-    }
-    console.log("Strains component", props.strains)
-    console.log("add to favorites", props.addToFavorites)
-    return(
-        <div>
-            <h1>Medical Cabinet Strains:</h1>
-            {props.strains.map(i => 
-            {return <div onClick={()=> {props.addToFavorites(i)}
-                
-            }>
-                 {/* <h5 className="strainNames"> Strain Name: {i.name} <br/> Strain Race: {i.race} </h5> */}
-                 
-            {/* <Link key={i.id} to={`/strains/${i.id}`}> */}
-            Strain Name: {i.name} <br/> Strain Race: {i.race}
-                {/* <Strain strain={i}/> */}
-            {/* </Link> */}
-             </div>})}
-            <Strain strains={props.strains} addToFavorites={props.addToFavorites}/>
-         {/* <button classname="fetch_button" onClick= {fetchStrains}>Strains</button> */}
-        </div>
-    )
-}
-
-const mapStateToProps = state =>{
-    console.log("State", state.strains)
-    return({
-        strains: state.strains,
-        error: state.error
-    })
-}
+// const mapStateToProps = state =>{
+//     console.log("State", state.strains)
+//     return({
+//         strains: state.strains,
+//         error: state.error
+//     })
+// }
 
 export default /*connect( mapStateToProps, { getStrains })*/ (Strains); 
